e9a1c5d70279e8a61f4703a281dc323592c146c1
NinuVJ <ninujohnson247@gmail.com>
2023-01-03 00:56:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css"
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+import { originals,action,horror } from "./urls";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title="Netflix Originals"/>
+      <RowPost url={action} title="Action" isSmall/>
+      <RowPost url={horror} title="Horror Movies" isSmall/>
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = 'cfc19ecff03ab90b055ada8dfb2c0b24';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
